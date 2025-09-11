7028811b8e3fd591f03e5907e97ea7c21c32d884
Juan Buitrago <buitr4go@gmail.com>
2025-09-11 10:20:11 -0500

    docker config added and backend build error fixed

--- proj/server/routes/user/user_routes.js
+++ proj/server/routes/user/user_routes.js
@@ -3,24 +3,24 @@ const ErrorHandler = require("../../utils/errorHandler.js")
 const router = express.Router();
 const axios = require("axios");
 const catchAsyncErrors = require("../../middleware/catchAsyncErrors.js");
-const User = require('../../models/userModel.js');
+// const User = require('../../models/userModel.js');
 
-const newUser = new User({
-  email: 'test@example.com',
-  password: 'hashedpassword', // Debes encriptarla antes de guardar
-});
+// const newUser = new User({
+//   email: 'test@example.com',
+//   password: 'hashedpassword', // Debes encriptarla antes de guardar
+// });
 
 //routes -> /user/
 router.get( 
     "/get-user",
     catchAsyncErrors(async (req, res, next) => {
       try {
-        await newUser.save();
-        console.log('Usuario guardado:', newUser);
-        res.status(201).json({
-            success: true,
-            data: "success",
-          });
+        // await newUser.save();
+        // console.log('Usuario guardado:', newUser);
+        // res.status(201).json({
+        //     success: true,
+        //     data: "success",
+        //   });
       } catch (error) {
         return next(new ErrorHandler(error.message, 500, "error on /user/get-user" ));
       }
